75bc88e7b00895479bf06eb8a5c95ee94758d4d1
Virendra Gupta <vgupta@ivycomptech.com>
2020-01-06 13:08:19 +0530

    first commit

---
+++ SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI
+//
+//  Created by Virendra Gupta on 1/6/20.
+//  Copyright © 2020 sample. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
